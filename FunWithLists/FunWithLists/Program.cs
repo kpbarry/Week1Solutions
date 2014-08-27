94686ce6694eddc9ced52459f9f67f4e6e72f107
SeedPaths-User <lessdismissive@email.com>
2014-08-27 16:54:49 -0600

    Update end of day

---
+++ FunWithLists/FunWithLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a new list of strings for a menu
+            List<string> myMenu = new List<string>(){"Pizza", "Tacos", "Hummus", "Instant Potatoes", "Twix"};
+
+            //Add one more menu item
+            myMenu.Add("Deep Fried Oreos");
+
+            //Remove something
+            myMenu.Remove("Pizza");
+            myMenu.RemoveAt(1);
+
+            //Another easy way to print out an array
+            Console.WriteLine(string.Join(", ", myMenu));
+
+            //Loop through and print each
+            for (int i = 0; i < myMenu.Count; i++)
+            {
+                //Set a variable equal to the current item in our list
+                string item = myMenu[i];
+                if (item.ToLower().Contains("twix"))
+                {
+                    Console.WriteLine(item.ToUpper());
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LoopingOverStrings/LoopingOverStrings/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingOverStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FindALetter("a", "WhoaaaaaaAa");
+            FindAWord("blargh", "I am a blargh zombie blargh");
+            Console.ReadKey();
+        }
+
+        static void FindALetter(string letterToFind, string text)
+        {
+            int letterCount = 0;
+
+            //Loop over the string
+            for(int i = 0; i < text.Length; i++)
+            {
+                //Convert char to string
+                string letter = text[i].ToString();
+                //Set letter to lower, check if it matches letterToFind
+                if (letter.ToLower().Contains(letterToFind))
+                {
+                    //If found, increase counter
+                    letterCount++;
+                }
+            }
+            Console.WriteLine("Found " + letterCount + " " + letterToFind + "'s" + " in " + text);
+        }
+        //Function to loop over words in a string
+        static void FindAWord(string wordToFind, string text)
+        {
+            //Counter
+            int wordCount = 0;
+            //Split string to words
+            List<string> words = text.Split(' ').ToList();
+            //Loop over the string
+            for (int i = 0; i < words.Count; i++)
+            {
+                string word = words[i];
+                //Set letter to lower, check if it matches letterToFind
+                if (word.ToLower() == wordToFind.ToLower())
+                {
+                    //If found, increase counter
+                    wordCount++;
+                }
+            }
+            Console.WriteLine("Found " + wordCount + " " + wordToFind + "'s" + " in " + text);
+        }
+    }
+}
