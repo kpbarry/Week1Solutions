ac77a8e68102ef12a5d22f154a09fad9d72c9f2d
SeedPaths-User <lessdismissive@email.com>
2014-08-27 14:41:42 -0600

    FizzBuzz

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -23,34 +23,43 @@ namespace Week_1_Code_Challenge
             }
 
             //Yodaizer
-            string reverseStr = "I am a potato";
+            string reverseStr = "I am Yoda";
             reverseStr = Yodaizer(reverseStr);
             Console.WriteLine(reverseStr);
 
             //TextStats
+            TextStats("Here is a three sentence sentence. I like writing sentences. They are great!");
 
-            //isPrime
+            //IsPrime
             for (int j = 0; j <= 25; j++)
             {
-                isPrime(j);
+                IsPrime(j);
             }
+
+            //DashInsert
+            DashInsert(8675309);
+
             Console.ReadKey();
         }
 
         static void FizzBuzz(int number)
         {
+            //This condition must be checked first to ensure a # is divisible by both #s
             if (number % 3 == 0 && number % 5 == 0)
             {
                 Console.WriteLine("FizzBuzz");
             }
+            //If divisible by 3 print Buzz
             else if (number % 3 == 0)
             {
                 Console.WriteLine("Buzz");
             }
+            //Not divisible by 3 or 5
             else if (number % 3 != 0 && number % 5 != 0)
             {
                 Console.WriteLine(number);
             }
+            //Divisible by 5
             else if (number % 5 == 0)
             {
                 Console.WriteLine("Fizz");
@@ -65,52 +74,66 @@ namespace Week_1_Code_Challenge
 
         }
 
-        //static int TextStats(string input)
-        //{
-        //    int consonants = 0, characters = 0, vowels = 0, special = 0, words = 0;
-
-        //    string splitUp = input;
-
-        //    for (int i = 0; i <= splitUp.Length; i++)
-        //    {
-        //        if (splitUp[i] == 'a' || splitUp[i] == 'e' || splitUp[i] == 'i' || splitUp[i] == 'o'
-        //            || splitUp[i] == 'u' || splitUp[i] == 'A' || splitUp[i] == 'E' ||
-        //            splitUp[i] == 'I' || splitUp[i] == 'O' || splitUp[i] == 'U')
-        //        {
-        //            vowels += 1;
-        //            characters += 1;
-        //        }
-        //        else if (splitUp[i] == ' ' || splitUp[i] == '.' || splitUp[i] == '!' ||
-        //            splitUp[i] == '?')
-        //        {
-        //            special = special + 1;
-        //            characters += 1;
-        //            if (splitUp[i] = ' ')
-        //            {
-        //                words += 1;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            consonants = splitUp.Length - speical - vowels;
-        //            characters += 1;
-        //        }
-        //        words = words + 1;
-        //        Console.WriteLine(characters, consonants, special, vowels, words);
-        //    }
-        //}
-
-        static void isPrime(int number)
+        static void TextStats(string input)
         {
+            int consonants = 0, vowels = 0, special = 0, words = 0;
+            int characters = input.Length;
+
+            foreach (char c in input)
+            {
+                if (!char.IsLetterOrDigit(c.ToString(), 0))
+                {
+                    special++;
+                }
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                    vowels++;
+                }
+                if (c == ' ')
+                {
+                    words++;
+                }
+            }
+            words = words + 1;
+            consonants = characters - special - vowels;
+
+            Console.WriteLine("Characters: " + characters + "\n Consonants: " + consonants + "\n Special: " + special + "\n Vowels: " 
+                + vowels + "\n Words: " + words);
+        }
+
+        //Increases divisor up to the value of the #, checking if they are divisors for 'number'
+        static void IsPrime(int number)
+        {
+            //Check to see if a # is divisible by another
             int divisor = 0;
+            //Can start at 2, since 1 is prime
             for (int i = 2; i <= number; i++)
                 if (number % i == 0)
+                    //Increase divisor to see if # is divisible
                     divisor++;
 
+            //Prime check - If divisor > 1, it's not prime
             if (number != 1 && divisor == 1)
                 Console.WriteLine(number + " is a prime number");
             else
                 Console.WriteLine(number);
+        }
+
+        static void DashInsert(int number)
+        {
+            string f = number.ToString();
+            //Will stop once it gets to the end of the string
+            for (int i = 0; i < f.Length; i++)
+            {
+                Convert.ToInt32(f[i]);
+                Console.Write(f[i]);
+                //Checks for 2 consecutive odd #s
+                if ((f[i] % 2 != 0) && (f[i + 1] % 2 != 0))
+                {
+                    Console.Write("-");
+                }
+            }
         }
     }
 }
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loop for fizzbuzz 1 => 35
+            for (int i = 1; i <= 35; i++)
+            {
+                FizzBuzz(i);
+            }
+            Console.ReadKey();
+        }
+
+        static void FizzBuzz(int number)
+        {
+            //This condition must be checked first to ensure a # is divisible by both #s
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            //If divisible by 3 print Buzz
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            //Divisible by 5
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            //Not divisible by 3 or 5
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
